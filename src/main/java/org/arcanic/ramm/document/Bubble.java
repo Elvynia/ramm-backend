44a5732a23660c86ea3977ef2c516216354e76cb
Elvynia <elvynia@gmail.com>
2016-11-10 12:21:09 +0100

    Corrected color and added random positions to root's children.

--- src/main/java/org/arcanic/ramm/document/Bubble.java
+++ src/main/java/org/arcanic/ramm/document/Bubble.java
@@ -2,11 +2,14 @@ package org.arcanic.ramm.document;
 
 import javax.persistence.Transient;
 
+import org.arcanic.ramm.util.Vector3;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "bubbles")
 public class Bubble extends MongoDocument {
 	public String content;
 	@Transient
-	public String color;
+	public int color;
+	@Transient
+	public Vector3 position;
 }
--- src/main/java/org/arcanic/ramm/service/MemoryService.java
+++ src/main/java/org/arcanic/ramm/service/MemoryService.java
@@ -1,8 +1,11 @@
 package org.arcanic.ramm.service;
 
+import java.util.Random;
+
 import org.arcanic.ramm.document.Bubble;
 import org.arcanic.ramm.document.Memory;
 import org.arcanic.ramm.repository.BubbleRepository;
+import org.arcanic.ramm.util.Vector3;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,10 +19,15 @@ public class MemoryService {
 		Memory memory = new Memory();
 		memory.root = new Bubble();
 		memory.root.content = "ROOT";
-		memory.root.color = "0xD0D0D0";
+		memory.root.color = 13684944;
+		memory.root.position = new Vector3();
 		memory.children = this.repository.findAll();
 		for (final Bubble child : memory.children) {
-			child.color = "0xFFD0D0";
+			child.color = 16765136;
+			int randX = (int) Math.floor(Math.random() * 100);
+			int randY = (int) Math.floor(Math.random() * 100);
+			int randZ = (int) Math.floor(Math.random() * 100);
+			child.position = new Vector3(randX, randY, randZ);
 		}
 		return memory;
 	}
